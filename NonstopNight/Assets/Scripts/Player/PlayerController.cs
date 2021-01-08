db33b536f03a79f1e6b7936c50cf0509eb7caeb9
Rayeternity <77100748+Rayeternity@users.noreply.github.com>
2021-01-08 19:14:22 +0800

    尝试制作角色移动

--- NonstopNight/Assets/Scripts/Player/PlayerController.cs
+++ NonstopNight/Assets/Scripts/Player/PlayerController.cs
@@ -4,18 +4,47 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    CharacterController player_charater_c;
+    public float speed = 1.0f;
+    private Vector3 move_direction = Vector3.zero;
     void Start()
     {
-        gameObject.GetComponent<Animation>().Play("idle");
+        player_charater_c = gameObject.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {            
-            PlayerMoveForward();
+        //if (Input.GetKey(KeyCode.W))
+        //{            
+        //    PlayerMoveForward();
+        //}
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    PlayerMoveForward();
+        //}
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    PlayerMoveForward();
+        //}
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    PlayerMoveForward();
+        //}
+        if (player_charater_c.isGrounded)
+        {
+
+            move_direction = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            move_direction *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                move_direction.y = 1;
+            }
+
+
+            // Move the controller
+            player_charater_c.Move(move_direction * Time.deltaTime);
         }
     }
 

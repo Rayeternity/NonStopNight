a4fde6f3480758566924779ef2fca79d4a00f4ac
Rayeternity <77100748+Rayeternity@users.noreply.github.com>
2021-01-24 16:23:17 +0800

    编辑角色动画，添加摄像机跟随

---
+++ NonstopNight/Assets/Scripts/Camera/CamaraFollow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraFollow : MonoBehaviour
+{
+    private Transform player_pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player_pos = GameObject.Find("STALKER").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move_pos =  new Vector3(0f, 7.2f, -4f);
+        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, move_pos + player_pos.position, Time.deltaTime*5);
+    }
+}
--- NonstopNight/Assets/Scripts/Player/PlayerController.cs
+++ NonstopNight/Assets/Scripts/Player/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     CharacterController player_charater_c;
     private Animator player_animator;
-    private float speed = 3f;
+    private float speed = 4f;
     private Vector3 move_direction = Vector3.zero;
     void Start()
     {
@@ -31,12 +31,26 @@ public class PlayerController : MonoBehaviour
         //}
         //}
         move_direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        player_animator.SetFloat("vertical",Input.GetAxis("Vertical"));       
+        //≤•∑≈“∆∂Ø∂Øª≠
+        Debug.Log("Horizontal" + Input.GetAxis("Horizontal"));
+        Debug.Log("Vertical" + Input.GetAxis("Vertical"));
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            player_animator.SetBool("move", true);
+        }
+        else
+        {
+            player_animator.SetBool("move", false);
+        }
+        
+
         player_charater_c.SimpleMove(move_direction *speed);
+        player_charater_c.transform.forward = Vector3.Lerp(transform.forward, move_direction, 0.1f);
+        //player_charater_c.transform.LookAt(transform.position + move_direction);
     }
 
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        Debug.Log(hit.gameObject.name);
+        //Debug.Log(hit.gameObject.name);
     }
 }
